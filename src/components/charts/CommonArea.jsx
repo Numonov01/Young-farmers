7c53138fcd6344f2210a60e26f253b407dfdd73b
Numonov01 <numonovtokhir@gmail.com>
2024-04-22 16:51:06 +0500

    first commit

--- src/components/charts/CommonArea.jsx
+++ src/components/charts/CommonArea.jsx
@@ -1,55 +1,123 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
-import { Pie } from "@ant-design/plots";
+import React, { useState } from "react";
+import { Radio, Table } from "antd";
 
 function CommonArea() {
-  const config = {
-    data: [
-      { type: "Andijon viloyati", value: 17 },
-      { type: "Buxoro viloyati", value: 15 },
-      { type: "Fargona viloyati", value: 18 },
-      { type: "Jizzax viloyati", value: 15 },
-      { type: "Xorazm viloyati", value: 10 },
-      { type: "Namangan viloyati", value: 5 },
-      { type: "Navoiy viloyati", value: 27 },
-      { type: "Qashqadaryo viloyati", value: 25 },
-      // { type: "Samarqand viloyati", value: 18 },
-      // { type: "Sirdaryo viloyati", value: 15 },
-      // { type: "Surxondaryo viloyati", value: 10 },
-      // { type: "Qoraqalpogiston Respublikasi", value: 5 },
-    ],
-    angleField: "value",
-    colorField: "type",
-    paddingRight: 80,
-    label: {
-      text: "value",
-      position: "outside",
+  const columns = [
+    {
+      title: "Region",
+      dataIndex: "name",
+      render: (text) => <a>{text}</a>,
     },
-    legend: {
-      color: {
-        title: false,
-        position: "right",
-        rowPadding: 5,
-      },
+    {
+      title: "Order",
+      dataIndex: "age",
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+    },
+  ];
+
+  const data = [
+    {
+      key: "1",
+      name: "Toshkent viloyati",
+      age: 12,
+      address: "Lake Park",
+    },
+    {
+      key: "2",
+      name: "Jizax viloyati",
+      age: 5,
+      address: "Lake Park",
+    },
+    {
+      key: "3",
+      name: "Andijon viloyati",
+      age: 9,
+      address: "Lake Park",
+    },
+    {
+      key: "4",
+      name: "Qashqadaryo viloyati",
+      age: 8,
+      address: "Lake Park",
+    },
+    {
+      key: "5",
+      name: "Samarqand viloyati",
+      age: 4,
+      address: "Lake Park",
+    },
+    {
+      key: "6",
+      name: "Navoi viloyati",
+      age: 7,
+      address: "Lake Park",
+    },
+  ];
+
+  const [selectionType, setSelectionType] = useState("checkbox");
+
+  const rowSelection = {
+    onChange: (selectedRowKeys, selectedRows) => {
+      console.log(
+        `selectedRowKeys: ${selectedRowKeys}`,
+        "selectedRows: ",
+        selectedRows
+      );
     },
   };
+
   return (
-    <div
-      className="map"
-      style={{
-        margin: 24,
-        minHeight: 360,
-        background: "white",
-        borderRadius: 8,
-      }}
-    >
-      <div style={{ paddingTop: "8px" }}>
-        <h3 style={{ padding: "18px" }}>Umumiy ajratilgan maydon</h3>
+    <>
+      <div
+        style={{
+          margin: 24,
+          minHeight: 360,
+          minWidth: 230,
+          background: "white",
+          borderRadius: 8,
+        }}
+      >
+        <h3 style={{ padding: 18 }}>Umumiy ajratilgan maydon</h3>
         <hr />
-        <br />
-        <Pie {...config} />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-around",
+            flexWrap: "wrap",
+            gap: 10,
+          }}
+        >
+          <div style={{ padding: 18, flex: 2 }}>
+            <img
+              src="../uzb.png"
+              alt="uzb"
+              style={{ maxWidth: "100%", height: "auto" }}
+            />
+          </div>
+          <div style={{ padding: 18, flex: 1 }}>
+            <Radio.Group
+              onChange={({ target: { value } }) => {
+                setSelectionType(value);
+              }}
+              value={selectionType}
+            ></Radio.Group>
+            <Table
+              rowSelection={{
+                type: selectionType,
+                ...rowSelection,
+              }}
+              columns={columns}
+              dataSource={data}
+            />
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
